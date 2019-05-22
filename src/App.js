84c235b05a8532ec82f44ac88494c3f947f1526e
Stutommi <tommi.teetee@hotmail.com>
2019-05-22 12:34:25 +0300

    started planning & working on layout with semantic UI'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Libraries
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import { Header } from 'semantic-ui-react'
 
-function App() {
+// Components
+import AppMenu from './components/Menu.js'
+
+const App = () => {
+  const [page, setPage] = useState('chat')
+  const [sensors, setSensors] = useState(null)
+  console.log('sensors', sensors)
+
+  useEffect(() => {
+    axios.get('http://86.115.57.126:8001/ws/pasila_sensors')
+      .then(response => console.log(response)
+      )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <AppMenu 
+    page={page}
+    />
+
+
+      <Header as='h1'>sensorData test</Header>
+      {sensors && sensors.map(sensor =>
+        <div key={sensor.id}>
+          <p>{sensor.id}</p>
+          <p>{sensor.type}</p>
+          <p>{sensor.temp}</p>
+          <p>{sensor.moisture}</p>
+        </div>
+      )
+      }
+    </>
+  )
 }
 
 export default App;
+
