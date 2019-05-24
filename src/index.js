f4d7282f93060602bdc6b2629a3b10300767a476
Stutommi <tommi.teetee@hotmail.com>
2019-05-24 13:09:09 +0300

    adding depencencies for apollo in clientside

--- src/index.js
+++ src/index.js
@@ -1,5 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+//Libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloProvider } from 'react-apollo-hooks'
+// ...Because of authorization for headers (instead of apollo-boost)
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
+// ...For Subscriptions (websocket)
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+// Components
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const wslink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: { reconnect: true }
+})
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('library-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wslink,
+  authLink.concat(httpLink),
+)
+
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root'))
