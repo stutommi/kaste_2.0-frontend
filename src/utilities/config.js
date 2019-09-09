44b8db3b17cc4cde44c0f68dffc399e9f2d74551
stutommi <tommi.teetee@hotmail.com>
2019-09-09 12:42:37 +0300

    fix url

--- src/utilities/config.js
+++ src/utilities/config.js
@@ -2,8 +2,8 @@ let wsUrl = 'ws://localhost:4000/graphql'
 let httpUrl = 'http://localhost:4000/graphql'
 
 if (process.env.NODE_ENV === 'production') {
-  wsUrl = 'wss://https://kaste.herokuapp.com/graphql'
-  httpUrl = 'https://https://kaste.herokuapp.com/graphql'
+  wsUrl = 'wss://kaste.herokuapp.com/graphql'
+  httpUrl = 'https://kaste.herokuapp.com/graphql'
 }
 
 export default { wsUrl, httpUrl }
