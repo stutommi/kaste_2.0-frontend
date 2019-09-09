05d7ff4a700fa17ad85cbfd9848d4c11e4e668cf
stutommi <tommi.teetee@hotmail.com>
2019-09-09 12:41:39 +0300

    change domain url to kaste.herokuapp.com

--- src/utilities/config.js
+++ src/utilities/config.js
@@ -2,8 +2,8 @@ let wsUrl = 'ws://localhost:4000/graphql'
 let httpUrl = 'http://localhost:4000/graphql'
 
 if (process.env.NODE_ENV === 'production') {
-  wsUrl = 'wss://lit-forest-54340.herokuapp.com/graphql'
-  httpUrl = 'https://lit-forest-54340.herokuapp.com/graphql'
+  wsUrl = 'wss://https://kaste.herokuapp.com/graphql'
+  httpUrl = 'https://https://kaste.herokuapp.com/graphql'
 }
 
 export default { wsUrl, httpUrl }
